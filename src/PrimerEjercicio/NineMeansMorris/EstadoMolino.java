b947492bcda0249fa251cddabea74bc039afae59
dmatiasr <matu_dmr@hotmail.com>
2016-06-20 12:52:35 -0300

    Representacion de un EstadoMolino

--- src/PrimerEjercicio/NineMeansMorris/EstadoMolino.java
+++ src/PrimerEjercicio/NineMeansMorris/EstadoMolino.java
@@ -10,71 +10,59 @@ import java.util.*;
 **/
 
 public class EstadoMolino implements AdversarySearchState {
-	private int[][] tablero;//simula el estado del tablero nineMensMorris
-	private int currentPlayer; //Jug1 = 1, Jug2= -1
+	private int currentPlayer; //Jug1 = 1, Jug2= 2
 	private EstadoMolino parent; //el padre del estado.
-	private int fila=7; //tamaño de filas
-	private int col=7;	//tamaño de columnas
-	private List<List<Integer>> vecinoCol; //Guarda por columna los vecinos
-	private List<List<Integer>> vecinoFil; //Guarda por fila los vecinos
-	private int nficha;
-	//Contructor de EstadoMolino para el Estado inicial
+	private Tablero tablero;
+    public Vecinos vecino;
+	public boolean esMolinoBool;//Almacena el valor de verdad si el estado es o no molino.
+    
+    //Contructor de EstadoMolino para el Estado inicial
 	public EstadoMolino(){
-		//representa al jugador corriente que esta jugando
-		currentPlayer=1;
-		nficha=0;//
-		//Inicializar Tablero con 0's
-		for (int f=0; f<fila;f++){
-			for(int c=0; c<col;c++){
-				tablero[f][c]=0;
-			}
-		}
-		parent=null;//para llevar quien es el padre del estado o 
-					//en todo caso que regla se aplico para generarlo.
-		//Seteo de vecinos de cada columna
-		//HACER !!!
-		vecinoCol= new LinkedList<Integer>();
-		vecinoFil= new LinkedList<Integer>(); 	
-	}
-	//Constructor de la clase con parametros
+        tablero= new Tablero(7,7); //tablero 7x7 con sus operaciones
+        vecino= new Vecinos(24,24);//matriz ady 24x24
+        currentPlayer=1; //Comienza el jugador numero 1
+	    parent=null;
+        esMolinoBool=false;
+    }
+	
+    //Constructor de la clase con parametros
 	//A partir de el se genera un nuevo estado.
 	//le paso como dato quien juega, donde juega, y de que estado viene
+    //Constructor para cuando se inserta una ficha
 	public EstadoMolino(int jug,int f,int c, EstadoMolino father){
-		//El control de si es correcta la posicion se hace desde el juego
-		tablero[f][c]= jug;//Donde juega 
-		currentPlayer=jug; //Seteo quien hizo la jugada.		
+		vecino.setFicha(jug,f);//jugada del jugador en posicion donde juega
+        tablero.refreshTab(vecino);//Actualiza el tablero con la jugada
+        currentPlayer=jug; //Seteo quien hizo la jugada.		
 		parent=father;//Y de que estado provino.
-		nficha++;
+        esMolinoBool= vecino.esMolino(f,jug);//Calcula si se genero molino con esa jugada
 	}
-/*
-	//ESTAS SON PARA EL CONTROL DEL JUEGO, NO VAN AQUI
-	//--------------------------------------------------------------
-	//Una posicion es apta en la matriz si es una posicion que respete
-	//la estructura del juego Nine Mens Morris
-	public boolean posicionApta(){
+    
+    //Constructor para cuando se genera molino por poner una ficha
+    //Entonces provee de la generacion de mas estados a partir de ello
+    //permitiendo eliminar al jugador corriente una ficha del contrario.
+	public EstadoMolino(int f,EstadoMolino father){
+        vecino.borraFicha(f);//Desocupa ese "nodo" f.
+        tablero.refreshTab(vecino);//Actualiza el tablero con la jugada
+        parent=father;
+    }
 
-	}
-	//una posicion esta Disponible en la matriz si, es posicionApta() y 
-	//ademas esa posicion no esta ocupado por un 1 o -1.
-	public boolean posicionDisponible(){
-
-	}
-	//-----------------------------------------------------------
-*/
-	//Retorna si el estado es Max, es decir, si esta jugando 
+    //Retorna si el estado es Max, es decir, si esta jugando 
 	//el jugador 1.
-	public boolean isMax(){
+    public boolean isMax(){
 		return (currentPlayer==1);
 	}
-	//retorna el tablero corriente
+	
+    //retorna el tablero corriente
 	public int[][] getTablero(){
-		return tablero;
+		return tablero.getTab();
 	}
-	//retorna el jugador corriente
+	
+    //retorna el jugador corriente
 	public int getPlayer(){
 		return currentPlayer;
 	}
-	//Retorna si dos estados son iguales, 
+	
+    //Retorna si dos estados son iguales, 
 	//Para que sean iguales, deben tener los mismos tableros,
 	//mismas componentes y debe estar jugando el mismo jugador. 
 	public boolean equals(AdversarySearchState other){
@@ -82,99 +70,30 @@ public class EstadoMolino implements AdversarySearchState {
 		boolean eq = ( (Arrays.deepEquals( st.getTablero(),this.getTablero() ) ) && (st.getPlayer()==this.getPlayer() ) );		
 		return eq;
 	}
+
 	//Retorna el estado del tablero en forma de String.
 	//con el jugador que esta jugando 
 	 public String toString() {
-        String s = "\n\t  ---------------------------- \n\t";
-        for (int f = 0; f < fila; f++) {
-            for (int c = 0; c < c; c++) {
-               s = s + " | "+tablero[f][c];
-            }
-            s = s + " | \n\t  ---------------------------- \n\t";
-        }
-        s = s + "   1   2   3   4   5   6   7\n";
+        String s= tablero.toString2();
         if (currentPlayer==1)
         	s+="Player 1 : "+currentPlayer;
-        if (currentPlayer==-1)
+        if (currentPlayer==2)
         	s+="Player 2 : "+currentPlayer;
-        if (currentPlayer==0)
-        	s+="No hay jugadas "+currentPlayer;
         return s;
      }
-
+    
      //Retorna el valor de verdad si el estado parametro es un molino
      //es molino si en la estructura del juego hay 3 mismas fichas alineadas
      //vertical u horizontalmente y ademas las fichas son "vecinas".
-     public boolean esMolino(EstadoMolino s){
-     	return false;
+     public boolean esMolino(){
+        return esMolinoBool;
      }
+    
      //retorna la cantida de fichas 
      public int getNumFicha(){
-     	return nficha;
+     	return vecino.cantFichas();
      }
-     //retorna la lista de los vecinos por columna
-     public List<List<Integer>> vecinosCol(){
-     	return vecinoCol;
-     }
-     //Retorna la lista de vecinos por Fila
-     public List<List<Integer>> vecinosFil(){
-     	return vecinosFil;
-     }
-
-
-     //Seteo de vecinos de cada columna en una Lista de listas
-     //Se hacer solo una vez para saber que vecinos tiene cada columna
-    public void seteoDeVecinos(){
-   		//addAll(int index, Collection<? extends E> c)
-     	List<Integer> list = new LinkedList<Integer>();
-   		//SETEO DE VECINOS POR COLUMNA
-   		//Para columna 0-- son vecinos    	
-    	
-     	/*list.add(0);
-     	list.add(3);
-     	list.add(6);
-     	vecinoCol.add(0,list);
-     	list.clear();
-     	//Para columna 1--
-     	list.add(1);
-     	list.add(3);
-     	list.add(5);
-     	vecinoCol.add(1,list);
-     	list.clear();
-     	//Para columna 2--
-     	list.add(2);
-     	list.add(3);
-     	list.add(4);
-     	vecinoCol.add(2,list);
-    	list.clear();
-    	//Para columna 3--Caso especial
-    	list.add(0);
-    	
-    	vecinoCol.add(3,0);
-    	vecinoCol.add(3,1);
-    	vecinoCol.add(3,2);
-    		//segunda parte
-    	vecinoCol.add(3,4);
-    	vecinoCol.add(3,5);
-    	vecinoCol.add(3,6);
-    	//Para columna 4
-    	vecinoCol.add(4,2);
-    	vecinoCol.add(4,3);
-    	vecinoCol.add(4,4);
-    	list.clear();
-    	//Para columna 5
-    	vecinoCol.add(5,1);
-    	vecinoCol.add(5,3);
-    	vecinoCol.add(5,5);
-    	list.clear();		
-    	//Para columna 6
-    	vecinoCol.add(6,0);
-    	vecinoCol.add(6,3);
-    	vecinoCol.add(6,6);
-    	list.clear();
-    //Seteo de vecinos por fila.
-*/
-    }
+ 
 
     //Retorna la regla Aplicada para llegar this.Estado 
     //O quien es el padre de este estado. 
